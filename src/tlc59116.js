f3bdf43d4a778e083c9cd5f25a4f30c395fbed3a
Nico De Witte <nico.dewitte@outlook.com>
2019-09-06 08:19:40 +0200

    Add class and constructor

--- src/tlc59116.js
+++ src/tlc59116.js
@@ -1,1 +1,10 @@
-console.log("Does nothing yet")
+class Tlc59116 {
+
+  constructor(i2c, devAddress=0x60) {
+    this.i2c = i2c;
+    this.devAddress = devAddress;
+  }
+
+}
+
+module.exports = Tlc59116;
