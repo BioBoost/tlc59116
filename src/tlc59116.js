8b0212b570f4cd47fe68014e9b984111c5d97197
Nico De Witte <nico.dewitte@outlook.com>
2019-09-07 07:43:38 +0200

    Add group blinking

--- src/tlc59116.js
+++ src/tlc59116.js
@@ -1,5 +1,6 @@
 const registers = {
   MODE1: 0x00,
+  MODE2: 0x01,
   PWM0: 0x02,
   GROUP_PWM: 0x12,
   GROUP_FREQ: 0x13,
@@ -10,6 +11,8 @@ class Tlc59116 {
   static LED_PER_LED_CONTROL = 0xAA;
   static GROUP_CONTROL = 0xFF;
   static LEDS = 16;
+  static MIN_PERIOD_MS = 41;
+  static MAX_PERIOD_MS = 10730;
 
   constructor(i2c, devAddress=0x60) {
     this.i2c = i2c;
@@ -41,7 +44,21 @@ class Tlc59116 {
   }
 
   group_dim(brightness) {
+    this.switch_group_control_to_dimming();
     this.i2c.writeByteSync(this.devAddress, registers.GROUP_PWM, brightness);
+  }
+
+  group_blink(period_ms, dutyCycle_percentage=50) {
+    this.switch_group_control_to_blinking();
+
+    period_ms = Math.max(Math.min(period_ms, Tlc59116.MAX_PERIOD_MS), Tlc59116.MIN_PERIOD_MS);
+    let freq = Math.floor(255 * (period_ms - Tlc59116.MIN_PERIOD_MS) / (Tlc59116.MAX_PERIOD_MS-Tlc59116.MIN_PERIOD_MS));
+
+    dutyCycle_percentage = Math.max(Math.min(dutyCycle_percentage, 100), 0);
+    let dc = Math.floor(255 * dutyCycle_percentage / 100.0);
+
+    this.i2c.writeByteSync(this.devAddress, registers.GROUP_FREQ, freq);
+    this.i2c.writeByteSync(this.devAddress, registers.GROUP_PWM, dc);
   }
 
   //////////////////////
@@ -52,6 +69,14 @@ class Tlc59116 {
     let register = this.auto_increment_reg_address(registers.LEDOUT0);
     let bytes = Buffer.alloc(4, mode);
     this.i2c.writeI2cBlockSync(this.devAddress, register, bytes.length, bytes);
+  }
+
+  switch_group_control_to_dimming() {
+    this.clear_single_bit(registers.MODE2, 5);
+  }
+
+  switch_group_control_to_blinking() {
+    this.set_single_bit(registers.MODE2, 5);
   }
 
   set_single_bit(register, bitPosition) {
